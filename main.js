31f47e8ab6b9299fed77f6d87fd89921c617d99c
c3h3 <c3h3.tw@gmail.com>
2015-05-24 11:35:09 +0800

    template if

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-
 console.log("this is both sides");
+Message = new Mongo.Collection("message");
+
+sampleMesssages = [
+  {text:"Hi!Meteor! (m1)"},
+  {text:"Agilearning.IO is awesome! (m2)"},
+  {text:"Agilearning.IO is so nice! (m3)"}
+]
+
+
 
 if (Meteor.isClient){
   console.log("this is client");
 
-  Template.message.helpers({
-    test:"test"
-  })
 
   Template.body.helpers({
-    testArray: _.range(0,10),
-    testObject:{
-      test:"test123"
-    },
-    testDate: new Date,
-    Msgs:[
-      {n:1,text:"Hi!Meteor! (m1)"},
-      {n:2,text:"Agilearning.IO is awesome! (m2)"},
-      {n:3,text:"Agilearning.IO is so nice! (m3)"}
-    ]
+    Msgs: sampleMesssages
+    // Msgs:[
+    //   {n:1,text:"Hi!Meteor! (m1)"},
+    //   {n:2,text:"Agilearning.IO is awesome! (m2)"},
+    //   {n:3,text:"Agilearning.IO is so nice! (m3)"}
+    // ]
   })
 
 }
